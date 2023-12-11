8987d56f29c268c3970be9366aac2355a09754de
Alexei Ralea <zuner.hunter@gmail.com>
2023-12-11 13:08:28 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Menu from './components/Menu';
+import ListaTari from './components/ListaTari';
+import Tara from './components/Tara';
+import Acasa from './components/Acasa';
+const App = () => {
+  const [tari, setTari] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://restcountries.com/v2/all');
+        const data = await response.json();
+        const countries = data.filter(country => country.name.length > 10);
+        setTari(countries);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Menu />}>
+            <Route index element={<Acasa />} />
+            <Route path = "tari/" element={<ListaTari tari={tari} />} />
+            <Route path="/tara/:nume" element={<Tara tari={tari} />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
